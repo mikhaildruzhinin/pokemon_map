207a01b8029e282d9e08b570259b319080864d51
Mikhail Druzhinin <druzhininmy@gmail.com>
2020-05-08 15:48:21 +0300

    multiple fixes
    * simplified dict creation
    * added an empty string as a default value for the string-based fields
    * minor fixes to the model names

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,23 +3,23 @@ from django.db import models
 class Pokemon(models.Model):
     name = models.CharField(max_length=200, verbose_name='Название', null=False, blank=False)
     image = models.ImageField(verbose_name='Изображение', null=False, blank=False)
-    description = models.TextField(max_length=500, verbose_name='Описание', null=False, blank=True)
-    name_en = models.CharField(max_length=200, verbose_name='Название (англ.)', null=False, blank=True)
-    name_jp = models.CharField(max_length=200, verbose_name='Название (яп.)', null=False, blank=True)
-    previous_evolution = models.ForeignKey('self', related_name = 'next', verbose_name='Предыдущая эволюция',
+    description = models.TextField(max_length=500, default="", verbose_name='Описание', null=False, blank=True)
+    name_en = models.CharField(max_length=200, default="", verbose_name='Название (англ.)', null=False, blank=True)
+    name_jp = models.CharField(max_length=200, default="", verbose_name='Название (яп.)', null=False, blank=True)
+    previous_evolution = models.ForeignKey('self', related_name = 'next_pokemons', verbose_name='Предыдущая эволюция',
                                             on_delete=models.PROTECT, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
 class PokemonEntity(models.Model):
-    Pokemon = models.ForeignKey(Pokemon, verbose_name='Покемон', on_delete=models.PROTECT, null=False, blank=False)
-    Latitude = models.FloatField(verbose_name='Широта', null=False, blank=False)
-    Longitude = models.FloatField(verbose_name='Долгота', null=False, blank=False)
+    pokemon = models.ForeignKey(Pokemon, related_name='pokemon_entities', verbose_name='Покемон', on_delete=models.PROTECT, null=False, blank=False)
+    latitude = models.FloatField(verbose_name='Широта', null=False, blank=False)
+    longitude = models.FloatField(verbose_name='Долгота', null=False, blank=False)
     appeared_at = models.DateTimeField(verbose_name='Появился в', null=True, blank=True)
     disappeared_at = models.DateTimeField(verbose_name='Исчезнет в', null=True, blank=True)
-    Level = models.IntegerField(verbose_name='Уровень', null=True, blank=True)
-    Health = models.IntegerField(verbose_name='Здоровье', null=True, blank=True)
-    Strength = models.IntegerField(verbose_name='Сила', null=True, blank=True)
-    Defence = models.IntegerField(verbose_name='Защита', null=True, blank=True)
-    Stamina = models.IntegerField(verbose_name='Выносливость', null=True, blank=True)
+    level = models.IntegerField(verbose_name='Уровень', null=True, blank=True)
+    health = models.IntegerField(verbose_name='Здоровье', null=True, blank=True)
+    strength = models.IntegerField(verbose_name='Сила', null=True, blank=True)
+    defence = models.IntegerField(verbose_name='Защита', null=True, blank=True)
+    stamina = models.IntegerField(verbose_name='Выносливость', null=True, blank=True)
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -46,28 +46,32 @@ def show_all_pokemons(request):
 
 def show_pokemon(request, pokemon_id):
     pokemon = Pokemon.objects.get(pk=pokemon_id)
-    pokemon_entities = PokemonEntity.objects.filter(Pokemon=pokemon)
+    pokemon_entities = PokemonEntity.objects.filter(pokemon=pokemon)
 
     folium_map = show_map(request, pokemon_entities)
 
-    pokemon_data = {}
-    previous_evolution = {}
-    next_evolution = {}
-    pokemon_data['title_ru'] = pokemon.name
-    pokemon_data['title_en'] = pokemon.name_en
-    pokemon_data['title_jp'] = pokemon.name_jp
-    pokemon_data['img_url'] = pokemon.image.url
-    pokemon_data['description'] = pokemon.description
+    pokemon_data = {
+        'title_ru': pokemon.name,
+        'title_en': pokemon.name_en,
+        'title_jp': pokemon.name_jp,
+        'img_url': pokemon.image.url,
+        'description': pokemon.description,   
+    }
+    
     if pokemon.previous_evolution is not None:
+        previous_evolution = {
+            'title_ru': pokemon.previous_evolution.name,
+            'pokemon_id': pokemon.previous_evolution.id,
+            'img_url': pokemon.previous_evolution.image.url,
+        }
         pokemon_data['previous_evolution'] = previous_evolution
-        previous_evolution['title_ru'] = pokemon.previous_evolution.name
-        previous_evolution['pokemon_id'] = pokemon.previous_evolution.id
-        previous_evolution['img_url'] = pokemon.previous_evolution.image.url
     try:
+        next_evolution = {
+            'title_ru': pokemon.next_pokemons.all()[0].name,
+            'pokemon_id': pokemon.next_pokemons.all()[0].id,
+            'img_url': pokemon.next_pokemons.all()[0].image.url,
+        }
         pokemon_data['next_evolution'] = next_evolution
-        next_evolution['title_ru'] = pokemon.next.all()[0].name
-        next_evolution['pokemon_id'] = pokemon.next.all()[0].id
-        next_evolution['img_url'] = pokemon.next.all()[0].image.url
     except IndexError:
         pass
 
@@ -79,10 +83,10 @@ def show_map(request, pokemon_entities):
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
 
     for pokemon_entity in pokemon_entities:
-        img_url = pokemon_entity.Pokemon.image.url
+        img_url = pokemon_entity.pokemon.image.url
         img_url = request.build_absolute_uri(location=img_url)
         add_pokemon(
-            folium_map, pokemon_entity.Latitude, pokemon_entity.Longitude,
-            pokemon_entity.Pokemon.name, img_url)
+            folium_map, pokemon_entity.latitude, pokemon_entity.longitude,
+            pokemon_entity.pokemon.name, img_url)
     return folium_map
 
